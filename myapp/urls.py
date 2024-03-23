a54e8b32e25e40f2308b9b6fa8dd713aaf94773a
Adrian Gonzalez <139197746+AdrianGonzalez878@users.noreply.github.com>
2024-03-23 21:44:10 +0000

    done

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("",views.home, name="home"),
-  path("about",views.about, name="about"),
+  path("",views.HomeView.as_view(), name="home"),
+  path("about/",views.AboutView.as_view(), name="about"),
+   path("movies/",views.MoviesView.as_view(), name="movies"),
+   path("python/",views.PythonView.as_view(), name="python"),
+  path('class/', views.ClassView.as_view(), name='class_view'),
+  path('theme/', views.ThemeView.as_view(), name='theme'),
+  path('load/', views.load_default_data_view, name='load_default_data'),
+  path('inventions/', views.InventionListView.as_view(), name='invention-list'),
+  path('invention/<int:pk>/', views.InventionDetailView.as_view(), name='invention-view'),
+  path('invention/create/', views.InventionCreateView.as_view(), name='create_invention'),
+  path('invention/<int:pk>/update/', views.InventionUpdateView.as_view(), name='update_invention'),
+  path('invention/<int:pk>/delete/', views.InventionDeleteView.as_view(), name='delete_invention'),
+
+
 ]
